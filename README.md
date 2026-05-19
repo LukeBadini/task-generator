e0c86e3a6f25d3e20a3c3f0c584b58497a17c166
LukeBadini <luke.badini@gmail.com>
2026-05-19 17:42:57 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Task Generator
+Generates a random task for account progression
--- src/test/java/com/taskgenerator/TaskGeneratorPluginTest.java
+++ src/test/java/com/taskgenerator/TaskGeneratorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.taskgenerator;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TaskGeneratorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TaskGeneratorPlugin.class);
 		RuneLite.main(args);
 	}
 }
